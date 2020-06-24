4f77a49f783afaa74904f480049a99f15ecf02f7
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2020-06-25 01:51:21 +0530

    setting env variable - $env:PORT = 5000

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,14 +5,15 @@ app.get('/', (req, res) => {
     res.send("Hello World!!!")
 })
 
-
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3])
 })
 
+// PORT
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+app.listen(port, () => {
+    console.log('Listening on port: ' + port)
 })
 
 // // HTTP methods
