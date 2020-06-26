a997c63e56b74a0d96f4117441a7479aa52df209
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2020-06-27 03:27:58 +0530

    Organising routes in modules

--- 4middleware/index.js
+++ 4middleware/index.js
@@ -1,8 +1,10 @@
 const config = require('config')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const course = require('./routes/courses')
+const home = require('./routes/home')
 const Joi = require('joi')
-const { authenticater, logger } = require('./authenticater')
+const { authenticater, logger } = require('./middleware/authenticater')
 const express = require('express')
 
 const app = express()
@@ -21,74 +23,12 @@ app.set('views', './views')  // Default
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-// if (app.get('env') === "development")
-//     app.use(morgan('tiny'))
+app.use(logger)
+app.use(authenticater)
 
-// // Custom Middleware
-// app.use(logger)
-
-// app.use(authenticater)
-
-const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
-]
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'My express app', message: 'Hello' })
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send("No course id found for fiven ID.")
-    res.send(course)
-})
-
-app.post('/api/courses', (req, res) => {
-
-    const { error } = validateCourse(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(courses)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send("No course id found for given ID.")
-
-    const { error } = validateCourse(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-
-    course.name = req.body.name
-    res.send(course)
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send("No course id found for given ID.")
-
-    const index = courses.indexOf(courses);
-    courses.splice(index, 1)
-
-    res.send(courses)
-})
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-    return Joi.validate(course, schema);
-}
+// Route
+app.use('/', home)
+app.use('/api/courses', course)
 
 const port = 3000
 app.listen(port, () => {
