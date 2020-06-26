785179b9447fa953c24b07d5ff63017a6d6a060d
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2020-06-27 02:31:46 +0530

    Debug

--- 5debugging/index.js
+++ 5debugging/index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require('debug')('app:startup')
+const dbDebugger = require('debug')('app:db')
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
@@ -5,10 +7,17 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+// set DEBUG=app:startup
+// set DEBUG=app:db
+// set DEBUG=app:startup,app:db
+// set DEBUG=app:*
+
 if (app.get('env') === "development") {
     app.use(morgan('tiny'))
-    console.log('Morgan enabled...');
+    startupDebugger('Morgan enabled...');
 }
+dbDebugger('DB connected...');
 
 const port = 3000
 app.listen(port, () => {
